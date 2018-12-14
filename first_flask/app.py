f4b5ccf2b21dac171a6a260eb88d7068bb278b10
Aibentao <vadonical@gmail.com>
2018-12-14 21:33:08 +0800

    add first_flask

---
+++ first_flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/')
+def index():
+    return 'This is Index Page!'
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/reg')
+def reg():
+    return redirect('/login')
+
+
+app.run(host='0.0.0.0', port=9527, debug=True)
+
+
---
+++ first_flask/test.py
@@ -0,0 +1,10 @@
+import datetime
+
+a = 'jjjj'
+b = {1, 2}
+
+import rest_framework
+
+print(rest_framework.__version__)
+
+import rest_framework
